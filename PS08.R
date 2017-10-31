c1cec23ee2fa9a53b71a039334f6b30d2334731d
kiryukawahata <kk15@hampshire.edu>
2017-10-31 08:23:30 -0400

    Submission

--- PS08.R
+++ PS08.R
@@ -1,5 +1,6 @@
 library(tidyverse)
 library(caret)
+library(MLmetrics)
 
 # Package for easy timing in R
 library(tictoc)
@@ -30,8 +31,9 @@ dim(train)
 model_formula <- as.formula(Device ~ X + Y + Z)
 
 # Values to use:
-n_values <- c(10, 20, 30)
-k_values <- c(2, 3, 4)
+n_values <- c(1:15000)
+k_values <- c(1:200)
+
 
 runtime_dataframe <- expand.grid(n_values, k_values) %>%
   as_tibble() %>%
@@ -41,20 +43,44 @@ runtime_dataframe
 
 
 
+train1 <- train %>% 
+  sample_n(15000)
+
+#n set to 15,000, k set to 200 max.
 
 # Time knn here -----------------------------------------------------------
 
+tic()
 
+for (i in 1:200) {
+  
+  
+  model_knn <- caret::knn3(model_formula, data = train1, k = i)
+  
+  fitted_matrix <- model_knn %>% 
+    predict(newdata = train1, type = "prob") %>% 
+    round(3)
+}
 
+timer_info <- toc()
+
+runtime <- timer_info$toc - timer_info$tic
+runtime
+
+fitted_tidy <- fitted_matrix %>% 
+  as_tibble()  
 
 # Plot your results ---------------------------------------------------------
 # Think of creative ways to improve this barebones plot. Note: you don't have to
 # necessarily use geom_point
+
 runtime_plot <- ggplot(runtime_dataframe, aes(x=n, y=k, col=runtime)) +
-  geom_point()
+  geom_line() + ggtitle("Runtime")
 
 runtime_plot
 ggsave(filename="firstname_lastname.png", width=16, height = 9)
+
+
 
 
 
@@ -64,6 +90,6 @@ ggsave(filename="firstname_lastname.png", width=16, height = 9)
 # of:
 # -n: number of points in training set
 # -k: number of neighbors to consider
-# -d: number of predictors used? In this case d is fixed at 3
+# -p: number of predictors used? In this case d is fixed at 3
 
-
+#Big-O runtime for single instance of knn = (n choose p) (k*n)*p
